3df0819e72c652b50454e8dff9955d3fbd0884e3
Luis Alvarez <mrluisfeer@gmail.com>
2026-04-05 12:42:03 -0600

    refactor: remove deprecated files and update configuration for LazyVim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,21 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
-
-local api = vim.api
-
--- Turn of paste mode when leaving insert
-api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
-})
-
--- Fix conceallevel for json files
-api.nvim_create_autocmd("FileType", {
-	pattern = { "json", "jsonc" },
-	callback = function()
-		local wo = vim.wo
-		wo.spell = false
-		wo.conceallevel = 0
-	end,
-})
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
